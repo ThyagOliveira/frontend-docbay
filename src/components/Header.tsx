d4130970ae678cb63fae04399722b9c9d24759fc
ThyagOliveira <taothyago@gmail.com>
2025-06-27 19:17:29 +0100

    Add header and footer components

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/components/Header.scss'
+
+export const Header: React.FunctionComponent = () => {
+  return (
+    <header className="header">
+      <div className="logo">DocBay Challenge</div>
+    </header>
+  )
+}
